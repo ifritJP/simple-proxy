5ef2590bc7216befbeb9df96eb44057ff9be1e15
ifritJP <15117169+ifritJP@users.noreply.github.com>
2021-12-02 07:14:14 +0900

    add the ip option and the forwardAuth option.

--- server.go
+++ server.go
@@ -15,16 +15,28 @@ import (
 	"github.com/elazarl/goproxy"
 )
 
+// forwardProxy に渡す auth 情報
+// なし
+const ForwardProxyAuthModeNone = 0
+// この proxy と同じ
+const ForwardProxyAuthModePass = 1
+// 別に指定
+const ForwardProxyAuthModeSpec = 2
+
+// proxy-authorization ヘッダ名
+const ProxyAuthHeaderName = "proxy-authorization"
+
+
 type AuthProxyServer struct {
-	// proxy 処理本体 (connect)
-	proxyForConnect *goproxy.ProxyHttpServer
-	// proxy 処理本体 (非connect)
-	proxyForNormal *goproxy.ProxyHttpServer
 	// basic 認証の情報 -> true の map
 	idPassBasicMap map[string]bool
 	// forwardProxy
 	forwardProxy   string
 	privateForward bool
+    forwardProxyAuthMode int
+    forwardProxyAuth string
+    // 受け入れ可能な IP
+    acceptableIPList []net.IPNet
 }
 
 func setupGoproxy(proxy *goproxy.ProxyHttpServer) {
@@ -36,18 +48,14 @@ func setupGoproxy(proxy *goproxy.ProxyHttpServer) {
 }
 
 func newAuthProxyServer(
-	port int, forwardProxy string, user string, privateForward bool) *AuthProxyServer {
-	proxyForConnect := goproxy.NewProxyHttpServer()
-	proxyForNormal := goproxy.NewProxyHttpServer()
-
-	setupGoproxy(proxyForConnect)
-	setupGoproxy(proxyForNormal)
-
+	port int, forwardProxy string, user string, privateForward bool,
+    forwardProxyAuthMode int, forwardProxyAuth string,
+    acceptableIPList []net.IPNet ) *AuthProxyServer {
 	log.Printf("forwardProxy: %s\n", forwardProxy)
 	authProxyServer := AuthProxyServer{
-		proxyForConnect, proxyForNormal,
 		map[string]bool{}, forwardProxy, privateForward,
-	}
+        forwardProxyAuthMode, forwardProxyAuth, acceptableIPList,
+    }
 	if user != "" {
 		auth := base64.StdEncoding.EncodeToString([]byte(user))
 		authProxyServer.idPassBasicMap[auth] = true
@@ -71,6 +79,22 @@ func (proxy *AuthProxyServer) checkAuth(auth string) string {
 	return ""
 }
 
+// モードに応じて proxy 認証情報を設定
+//
+// @param forwardReq forward 先の proxy の request
+// @param req forward 元の request
+func (proxy *AuthProxyServer) setProxyAuth( forwardReq, req *http.Request ) {
+    if proxy.forwardProxyAuthMode == ForwardProxyAuthModeNone {
+        // proxy 認証情報を forwardProxy に渡さない
+    } else if proxy.forwardProxyAuthMode == ForwardProxyAuthModePass {
+        // この proxy と同じ認証情報を使用する
+        forwardReq.Header.Add( ProxyAuthHeaderName, req.Header.Get(ProxyAuthHeaderName) )
+    } else if proxy.forwardProxyAuthMode == ForwardProxyAuthModeSpec {
+        // 所定の proxy 認証情報を使用する
+        forwardReq.Header.Add( ProxyAuthHeaderName, "Basic " + proxy.forwardProxyAuth )
+    }
+}
+
 func (proxy *AuthProxyServer) forward(respWriter http.ResponseWriter, req *http.Request) int {
 	log.Printf("forward: %s\n", req.URL.String())
 
@@ -92,8 +116,13 @@ func (proxy *AuthProxyServer) forward(respWriter http.ResponseWriter, req *http.
 		log.Printf("NewRequest: %s", err)
 		return 500
 	}
-	forwardReq.Header = req.Header
+    // ヘッダ設定を一旦 clone し、proxy 認証情報 を削除する。
+    forwardReq.Header = req.Header.Clone()
+    forwardReq.Header.Del( ProxyAuthHeaderName )
 
+    // モードに応じて proxy 認証情報を設定
+    proxy.setProxyAuth( forwardReq, req )
+        
 	forwardResp, err := client.Do(forwardReq)
 	if err != nil {
 		log.Printf("Do: %s", err)
@@ -112,15 +141,39 @@ func (proxy *AuthProxyServer) forward(respWriter http.ResponseWriter, req *http.
 	return 0
 }
 
+func (proxy *AuthProxyServer) checkAccept( req *http.Request ) bool {
+    if len( proxy.acceptableIPList ) == 0 {
+        return true
+    }
+    addrTxt := req.RemoteAddr
+    if loc := strings.Index( addrTxt, ":" ); loc != -1 {
+        addrTxt = addrTxt[:loc]
+    }
+    
+    if remoteAddr := net.ParseIP( addrTxt ); remoteAddr != nil {
+        for _, acceptableIP := range( proxy.acceptableIPList ) {
+            if acceptableIP.Contains( remoteAddr ) {
+                return true
+            }
+        }
+    }
+    log.Printf( "reject access from %v", req.RemoteAddr )
+    return false
+}
+
 func (proxy *AuthProxyServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
-	log.Printf("RemoteAddr: %s", req.RemoteAddr)
+	log.Printf("RemoteAddr: %v, %s", req.URL.IsAbs(), req.RemoteAddr )
+    if !proxy.checkAccept( req ) {
+        resp.WriteHeader(400)
+        return
+    }
 
 	if len(proxy.idPassBasicMap) != 0 {
 		for key := range req.Header {
 			log.Printf("header = %s, val = %s", key, req.Header.Get(key))
 		}
-		authResult := proxy.checkAuth(req.Header.Get("Proxy-Authorization"))
+		authResult := proxy.checkAuth(req.Header.Get(ProxyAuthHeaderName))
 		if authResult != "" {
 			resp.Header().Add("Proxy-Authenticate", "Basic")
 			log.Printf(authResult)
@@ -134,27 +187,27 @@ func (proxy *AuthProxyServer) ServeHTTP(resp http.ResponseWriter, req *http.Requ
 		privateAccess = hostIp.IsPrivate()
 	}
 
+	aGoproxy := goproxy.NewProxyHttpServer()
+	setupGoproxy( aGoproxy )
+
 	if proxy.forwardProxy == "" {
 		// forwardProxy がない場合は、 goproxy に処理を任せる
 		log.Printf("proxy: %s", req.URL.String())
-		proxy.proxyForNormal.ServeHTTP(resp, req)
+		aGoproxy.ServeHTTP(resp, req)
 	} else if privateAccess && !proxy.privateForward {
 		// forwardProxy が指定されていても private アドレスアクセスの場合は、
 		// forwardProxy は使用しない。
 		log.Printf("skip forward: %s", req.URL.String())
-		proxy.proxyForNormal.ServeHTTP(resp, req)
+		aGoproxy.ServeHTTP(resp, req)
 	} else if req.Method == "CONNECT" {
 		// forwardProxy の指定があり CONNECT の場合は、
 		// Proxy-Authenticate を付けなおす。
 		log.Printf("forward CONNECT: %s", req.URL.String())
-		proxy.proxyForConnect.ConnectDial = proxy.proxyForConnect.NewConnectDialToProxyWithHandler(
+		aGoproxy.ConnectDial = aGoproxy.NewConnectDialToProxyWithHandler(
 			proxy.forwardProxy, func(forwardReq *http.Request) {
-				auth := req.Header.Get("Proxy-Authorization")
-				if auth != "" {
-					forwardReq.Header.Add("Proxy-Authorization", auth)
-				}
+                proxy.setProxyAuth( forwardReq, req )
 			})
-		proxy.proxyForConnect.ServeHTTP(resp, req)
+		aGoproxy.ServeHTTP(resp, req)
 	} else {
 		// 通常の http 処理で、 forwardProxy が設定されている場合は、
 		// ここで forward する。
@@ -181,6 +234,11 @@ func main() {
 	userOpt := cmd.String("user", "", "proxy id:pass. e.g. id=123, pass=abc, -user 123:abc")
 	forwardProxy := cmd.String("forward", "", "forward proxy (http://proxy.addr:port/). pass this auth.")
 	privateForward := cmd.Bool("pf", false, "When host is private address, it uses forwarding.")
+    forwardProxyAuthModeStr := cmd.String(
+        "forwardAuth", "pass",
+        "forward proxy auth mode. \n   - none\n   - pass\n   - spec:id:pass\n" )
+    acceptableIP := cmd.String(
+        "aip", "", "accepable IP. \n  e.g. 192.168.0.1/24\n       192.168.0.1/32" )
 
 	cmd.Parse(os.Args[1:])
 
@@ -192,7 +250,29 @@ func main() {
 		cmd.Usage()
 	}
 
-	proxy := newAuthProxyServer(port, *forwardProxy, *userOpt, *privateForward)
+    forwardProxyAuthMode := ForwardProxyAuthModeNone
+    forwardProxyAuth := ""
+    if *forwardProxyAuthModeStr == "none" {
+        forwardProxyAuthMode = ForwardProxyAuthModeNone
+    } else if *forwardProxyAuthModeStr == "pass" {
+        forwardProxyAuthMode = ForwardProxyAuthModePass
+    } else if strings.Index( *forwardProxyAuthModeStr, "spec:" ) == 0 {
+        forwardProxyAuthMode = ForwardProxyAuthModeSpec
+        auth := (*forwardProxyAuthModeStr)[ len( "spec:" ): ]
+        forwardProxyAuth = base64.StdEncoding.EncodeToString([]byte(auth))
+    } else {
+        cmd.Usage()
+    }
+
+    acceptableIPList := []net.IPNet{}
+    if _, ipnet, err := net.ParseCIDR( *acceptableIP ); err == nil {
+        log.Printf( "aip %v", *ipnet )
+        acceptableIPList = append( acceptableIPList, *ipnet )
+    }
+
+	proxy := newAuthProxyServer(
+        port, *forwardProxy, *userOpt, *privateForward,
+        forwardProxyAuthMode, forwardProxyAuth, acceptableIPList )
 
 	log.Print("start -- ", port)
 
